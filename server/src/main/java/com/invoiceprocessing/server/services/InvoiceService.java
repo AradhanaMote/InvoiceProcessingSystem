0581a3adf6b5b41e3078b8df10a2c6d8820c7b8c
AradhanaMote <aradhana2434@gmail.com>
2026-01-17 10:29:11 +0530

    some files added through intelliJ

--- server/src/main/java/com/invoiceprocessing/server/services/InvoiceService.java
+++ server/src/main/java/com/invoiceprocessing/server/services/InvoiceService.java
@@ -1,2 +1,7 @@
-package com.invoiceprocessing.server.services;public interface InvoiceService {
+package com.invoiceprocessing.server.services;
+
+import com.invoiceprocessing.server.model.Invoice;
+
+public interface InvoiceService {
+    public Invoice addInvoice(Invoice invoice);
 }
